17860641003bcadd79fd93b34c75018d45d168fe
deiviz <tangerdream@gmail.com>
2022-05-10 11:13:26 +0200

    refactor Game.vue on /lib folder with damageEvents and BuildEventsDeck

---
+++ src/lib/damageEvents.js
@@ -0,0 +1,18 @@
+
+export const DamageEvents = () => {
+  const basic = {
+    level1: { level: 1, name: 'Damage on one location', type: 'damage-location', value: 1 },
+    level2: { level: 2, name: 'Damage on two locations', type: 'damage-location', value: 2 },
+    level3: { level: 3, name: 'Damage on three locations', type: 'damage-location', value: 3 },
+  }
+
+  const special = [
+    { name: 'No Damage', type: 'damage-location', value: 0 },
+    { name: 'Tu traje tiene una rotura - You have to fix it', type: 'damage-player', value: { num: 1, type: 'data'}}
+  ]
+
+  return {
+    basic,
+    special
+  }
+}
